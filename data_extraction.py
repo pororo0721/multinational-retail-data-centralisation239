f81b9478e3ed20ba2b5c8850118b83d220094020
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-13 13:53:57 +0000

    Create data_extraction.py

---
+++ data_extraction.py
@@ -0,0 +1,15 @@
+class DataExtractor:
+    @staticmethod
+    def extract_csv(file_path):
+        # Method to extract data from CSV file
+        pass
+
+    @staticmethod
+    def extract_api(api_url):
+        # Method to extract data from an API
+        pass
+
+    @staticmethod
+    def extract_s3(bucket_name, object_key):
+        # Method to extract data from an S3 bucket
+        pass
