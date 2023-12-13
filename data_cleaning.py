e1cf97a83db445318c33dd823fc2e1d958a92bac
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-13 14:06:35 +0000

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,15 @@
+class DataCleaning:
+    @staticmethod
+    def clean_csv(data):
+     # Method to clean data extracted from CSV   
+        pass
+
+    @staticmethod
+    def clean_api_data(data):
+    # Method to clean data extracted from an API
+        pass
+
+    @staticmethod
+    def clean_s3_data(data):
+    # Method to clean data extracted from an S3 bucket  
+        pass
