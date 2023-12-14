8802b7723d0a63dd24073aa56d83b2032ea5a747
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-14 14:23:36 +0000

    Update database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -9,5 +9,16 @@ class DatabaseConncector:
         return db_creds
 
     
+    def init_db_engine(self):
+        db_creds=self.read_db_creds()
+        engine= create_engine(f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        return engine
 
-        
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        tables = engine.tables_names()
+        return tables
+
+    def upload_to_db(self, df, tables_names):
+        # Implement logic to upload DataFrame to the specified database table
+        pass        
