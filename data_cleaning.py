1f7c248fecc85bcf63c6e71f147b1b2469afb048
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-18 11:23:12 +0000

    update card_details.pdf and data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 class DataCleaning:
+    def clean_card_data(df):
+    # Implement your data cleaning logic for card data here
+    # Remove erroneous values, NULL values, or errors with formatting
+
+    # Placeholder: Drop rows with any NULL values
+        cleaned_df = df.dropna()
+
+        return cleaned_df 
+
     @staticmethod
     def clean_csv(data):
      # Method to clean data extracted from CSV   
