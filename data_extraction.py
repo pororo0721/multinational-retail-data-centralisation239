b87a8c54141b47aaea41dfc5904ac75cb224f3c5
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-18 11:13:01 +0000

    add retrieve_pdf_data

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import tabula
 
 class DataExtractor:
     
@@ -6,6 +7,21 @@ class DataExtractor:
         query = f"SELECT * FROM {table_name}"
         data = pd.read_sql(query, db_connector.conn)
         return data
+
+    def retrieve_pdf_data(pdf_link):
+        # Method to extract data from a PDF using tabula-py
+        try:
+            # Use tabula to extract tables from the PDF
+            tables= tabula.read_pdf(pdf_link, page='all', multiple_tables=True)
+
+            # Combine tables into a single DataFrame
+            df= pd.concat(tables, ignore_index=True)
+
+            return df
+
+        except Exception as e:
+            print(f"Error extracting data from PDF: {e}")
+            return None     
 
     @staticmethod
     def extract_csv(file_path):
