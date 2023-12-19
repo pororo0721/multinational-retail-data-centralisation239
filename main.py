e12bb4504a8a4478ece4abfe9397fb68b5ab7aed
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-12-19 11:37:52 +0000

    add retrieving store data from the API

--- main.py
+++ main.py
@@ -26,19 +26,23 @@ data_extractor = DataExtractor()
 # db_connector.disconnect()
 
 # Step 7: Extract card data from PDF
-pdf_link_card=r"C:\Users\Sinhye\Documents\GitHub\multinational-retail-data-centralisation239\card_details.pdf"
-card_data_extractor= DataExtractor()
-card_data= card_data_extractor.retrieve_pdf_data(pdf_link_card)
+# pdf_link_card=r"C:\Users\Sinhye\Documents\GitHub\multinational-retail-data-centralisation239\card_details.pdf"
+# card_data_extractor= DataExtractor()
+# card_data= card_data_extractor.retrieve_pdf_data(pdf_link_card)
 
 # Step 8: Clean the card data
-card_data_cleaning= DataCleaning()
-cleaned_card_data= card_data_cleaning.clean_card_data(card_data)
+# card_data_cleaning= DataCleaning()
+# cleaned_card_data= card_data_cleaning.clean_card_data(card_data)
 
 # Step 9: Upload cleaned card data to the database
-db_uploader=DatabaseUploader()
-db_uploader.upload_card_data(cleaned_card_data, 'dim_card_details')
+# db_uploader=DatabaseUploader()
+# db_uploader.upload_card_data(cleaned_card_data, 'dim_card_details')
 
 # Step 10: Retrieve the number of stores from the API
 number_stores_endpoint=" https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
 headers={'x-api-key' : 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 list_number_of_stores= data_extractor.list_number_of_stores(number_stores_endpoint,headers)
+
+# Step 11: Retrieve store data from the API
+store_endpoint="https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
+store_data= data_extractor.retrieve_stores_data(store_endpoint, headers)
