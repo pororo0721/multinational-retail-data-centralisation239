27cf2ff2fdd1be0c8bb606ab4b558c519c807265
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2024-01-16 10:44:07 +0000

    Add task 2

--- milestone4.sql
+++ milestone4.sql
@@ -3,3 +3,9 @@ SELECT country, COUNT(store_id) AS total_no_stores
 FROM stores
 GROUP BY country
 ORDER BY total_no_stores DESC;
+
+-- Task 2:
+SELECT locality, COUNT(*) as total_no_stores
+FROM stores
+GROUP BY locality
+ORDER BY total_no_stores DESC;
